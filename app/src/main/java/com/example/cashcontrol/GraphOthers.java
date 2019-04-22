a802b6baf7064def2592bc9c3b549c353bdf6e03
Glitch101 <no1knowsmypwd>
2019-04-22 19:09:41 +0530

    Changed icon

--- app/src/main/java/com/example/cashcontrol/GraphOthers.java
+++ app/src/main/java/com/example/cashcontrol/GraphOthers.java
@@ -3,12 +3,9 @@ package com.example.cashcontrol;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-
-import static com.example.cashcontrol.Predict.food;
 import static com.example.cashcontrol.Predict.others;
 
 public class GraphOthers extends AppCompatActivity {
